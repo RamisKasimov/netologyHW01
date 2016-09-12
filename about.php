2c69bb75bc277462dcbe9715a7f46c4bd6e1f12d
RamisKasimov <rkasimov@yahoo.com>
2016-09-12 14:17:15 +0300

    about.php

---
+++ about.php
@@ -0,0 +1,38 @@
+<?php
+ini_set('default_charset', 'UTF-8');
+
+$user = 'Рамис';
+$age = '28';
+$mail = 'ramis-kasimov@rambler.ru';
+$location = 'Дербент';
+$about = 'Рекламщик, изучаю программирование';
+?>
+<html>
+    <head>
+        <style>
+            dl dt {
+                float: left;
+                width: 200px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Домашнее задание №1 пользователя <?= $user ?></h1>
+        <dl>
+            <dt>Имя</dt>
+            <dd><?= $user ?></dd>
+            
+            <dt>Возраст</dt>
+            <dd><?= $age ?></dd>
+            
+            <dt>Электронная почта</dt>
+            <dd><?= $mail ?></dd>
+            
+            <dt>Город</dt>
+            <dd><?= $location ?></dd>
+            
+            <dt>О себе</dt>
+            <dd><?= $about ?></dd>
+        </dl>
+    </body>
+</html>
