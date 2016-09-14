f26682b3a29a0a9223e8da9bef911a0e19f655c4
RamisKasimov <rkasimov@yahoo.com>
2016-09-14 20:32:41 +0300

    netDZ02.php

--- netDZ02.php
+++ netDZ02.php
@@ -8,26 +8,26 @@ ini_set('default_charset', 'UTF-8');
     </head>
     <body>
         <form method="GET" action="<?=$_SERVER['PHP_SELF']?>">
-            Введи число от 1 до 10 <input type="number" name="yourNumber">
+            Введи число от 1 до 5 <input type="number" name="yourNumber">
             <input type="submit">
         </form>
         <br/>
 <?php
-$a = rand (1,10);
-$b = $_GET['yourNumber'];
-if (isset($b)) {
-    if ($b>$a) {
-        echo "Много<br/>";
-        echo "Компьютер задумал $a<br/><br/>";
-    }
-    elseif ($b<$a) {
-        echo "Мало<br/>";
-        echo "Компьютер задумал $a<br/><br/>";
-    }
-    else {
-        echo "Вы угадали!<br/>";
-        echo "Компьютер задумал $a<br/><br/>";
-    }
+if (isset($_GET["yourNumber"])) {
+	$a = rand (1,5);
+	$b = $_GET["yourNumber"];
+	if ($b>$a) {
+		echo "Много<br/>";
+		echo "Компьютер задумал $a<br/><br/>";
+	}
+	elseif ($b<$a) {
+		echo "Мало<br/>";
+		echo "Компьютер задумал $a<br/><br/>";
+	}
+	else {
+		echo "Вы угадали!<br/>";
+		echo "Компьютер задумал $a<br/><br/>";
+	}
 }
 ?>
     </body>
